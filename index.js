21a01fe09a1c0562cb33ef431a9c3f7941ce0c53
anjankumarbatchu <121348643+anjankumarbatchu@users.noreply.github.com>
2023-01-31 14:36:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+
+const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+  const voicesDropdown = document.querySelector('[name="voice"]');
+  const options = document.querySelectorAll('[type="range"], [name="text"]');
+  const speakButton = document.querySelector('#speak');
+  const stopButton = document.querySelector('#stop');
+
+  speakButton.addEventListener("click", ()=>{
+    msg.rate = options[0].value;
+    msg.pitch = options[1].value;
+    msg.text = options[2].value;
+    window.speechSynthesis.speak(msg);
+  });
+
+  stopButton.addEventListener("click", ()=>{
+    window.speechSynthesis.cancel();
+  });
+
+
+
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
